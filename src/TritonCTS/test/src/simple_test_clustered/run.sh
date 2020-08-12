f71424cf28630b8598e538def69ac6aaf05ba279
Marcelo Freire Danigno <38264358+MarceloDanigno@users.noreply.github.com>
2020-08-11 23:29:57 -0300

    [TritonCTS] Clustering and parameter updates (#426)
    * [TritonCTS] Clustering and parameter updates
    
    * [TritonCTS] New unit test for multiple parameters
    
    * [TritonCTS] simple_test_clustered update
    
    * [TritonCTS] Reduce unit test size
    
    * [TritonCTS] New test def
    
    * [TritonCTS] Removed unnecessary sdc commands

--- src/TritonCTS/test/src/simple_test_clustered/run.sh
+++ src/TritonCTS/test/src/simple_test_clustered/run.sh
@@ -11,7 +11,7 @@ binary=$1
 
 $binary -no_init < run.tcl > test.log 2>&1
 
-if grep -q "The chacterization used 1 buffer(s) types. All of them are in the loaded DB." test.log;
+if grep -q "Fanout distribution for the current clock = 2:4, 3:3, 4:3, 5:2, 6:6, 7:2, 8:4, 9:3, 10:42, 12:1, 13:1." test.log;
 then
 	exit $GREEN
 else
