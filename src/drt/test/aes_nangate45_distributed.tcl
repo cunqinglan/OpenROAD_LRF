4be4809fbac2e13873ff06a5a0efab0668e6cbb3
osamahammad21 <osama21@aucegypt.edu>
2022-01-27 16:12:42 +0200

    drt: add aes_nangate45_distributed test drt: fix different results in distributed routing
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/drt/test/aes_nangate45_distributed.tcl
+++ src/drt/test/aes_nangate45_distributed.tcl
@@ -4,16 +4,16 @@ set OR $argv0
 set server1 [$OR server1.tcl > results/server1.log &]
 set server2 [$OR server2.tcl > results/server2.log &]
 set balancer [$OR balancer.tcl > results/balancer.log &]
-set base [$OR gcd_nangate45.tcl > results/base.log &]
+set base [$OR aes_nangate45.tcl > results/base.log &]
 
 read_lef Nangate45/Nangate45_tech.lef
 read_lef Nangate45/Nangate45_stdcell.lef
-read_def gcd_nangate45_preroute.def
+read_def aes_nangate45_preroute.def
 set_thread_count [exec getconf _NPROCESSORS_ONLN]
-detailed_route -guide gcd_nangate45.route_guide \
-               -output_guide results/gcd_nangate45_distributed.output.guide.mod \
-               -output_drc results/gcd_nangate45_distributed.output.drc.rpt \
-               -output_maze results/gcd_nangate45_distributed.output.maze.log \
+detailed_route -guide aes_nangate45.route_guide \
+               -output_guide results/aes_nangate45_distributed.output.guide.mod \
+               -output_drc results/aes_nangate45_distributed.output.drc.rpt \
+               -output_maze results/aes_nangate45_distributed.output.maze.log \
                -verbose 1 \
 	             -distributed \
 	             -remote_host 127.0.0.1 \
@@ -22,6 +22,6 @@ detailed_route -guide gcd_nangate45.route_guide \
 exec kill $server1
 exec kill $server2
 exec kill $balancer
-set def_file results/gcd_nangate45.def
+set def_file results/aes_nangate45.def
 write_def $def_file
-diff_files results/gcd_nangate45.defok $def_file
+diff_files results/aes_nangate45.defok $def_file
