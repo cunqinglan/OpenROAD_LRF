The-OpenROAD-Project/OpenROAD

commit c950307c1eeffebde769060e68e54340bca3b96e

dft: removing the usage of odb's isSequential
I am also removing one_cell_nangate45.tcl test as it seems that it was
not working as the golden logs show that no scan replacement was
performed.

Signed-off-by: Felipe Garay <fgaray@google.com>

src/dft/src/cells/ScanCellFactory.cpp
@@ -80,10 +80,11 @@ sta::TestCell* GetTestCell(odb::dbMaster* master,
   return nullptr;
 }
 
-TypeOfCell IdentifyCell(odb::dbInst* inst)
+TypeOfCell IdentifyCell(odb::dbInst* inst, sta::dbSta* sta)
 {
-  odb::dbMaster* master = inst->getMaster();
-  if (master->isSequential() && !master->isBlock()) {
+  sta::dbNetwork* db_network = sta->getDbNetwork();
+  sta::LibertyCell* liberty_cell = GetLibertyCell(inst->getMaster(), db_network);
+  if (liberty_cell->hasSequentials() && !inst->getMaster()->isBlock()) {
     // we assume that we are only dealing with one bit cells, but in the future
     // we could deal with multibit cells too
     return TypeOfCell::OneBitCell;
@@ -117,9 +118,16 @@ std::unique_ptr<ClockDomain> GetClockDomainFromClock(
 }
 
 std::unique_ptr<ClockDomain> FindOneBitCellClockDomain(odb::dbInst* inst,
-                                                       sta::dbSta* sta)
+                                                       sta::dbSta* sta, utl::Logger* logger)
 {
   std::vector<odb::dbITerm*> clock_pins = utils::GetClockPin(inst);
+  if (clock_pins.empty()) {
+    logger->warn(utl::DFT,
+                  49,
+                  "Can't find a clock pin for cell '{:s}'",
+                  inst->getName());
+    return nullptr;
+  }
   sta::dbNetwork* db_network = sta->getDbNetwork();
   // A one bit cell should only have one clock pin
 
@@ -140,7 +148,7 @@ std::unique_ptr<OneBitScanCell> CreateOneBitCell(odb::dbInst* inst,
 {
   sta::dbNetwork* db_network = sta->getDbNetwork();
   std::unique_ptr<ClockDomain> clock_domain
-      = FindOneBitCellClockDomain(inst, sta);
+      = FindOneBitCellClockDomain(inst, sta, logger);
   sta::TestCell* test_cell = GetTestCell(inst->getMaster(), db_network, logger);
 
   if (!clock_domain) {
@@ -175,7 +183,7 @@ std::unique_ptr<ScanCell> ScanCellFactory(odb::dbInst* inst,
                                           sta::dbSta* sta,
                                           utl::Logger* logger)
 {
-  TypeOfCell type_of_cell = IdentifyCell(inst);
+  TypeOfCell type_of_cell = IdentifyCell(inst, sta);
 
   switch (type_of_cell) {
     case TypeOfCell::OneBitCell:

src/dft/test/CMakeLists.txt
@@ -2,7 +2,6 @@ or_integration_tests(
   "dft"
   TESTS
     max_chain_count_sky130
-    one_cell_nangate45
     one_cell_sky130
     place_sort_sky130
     scan_architect_clock_mix_sky130

src/dft/test/one_cell_nangate45.ok
@@ -1,34 +1,0 @@
-[INFO ODB-0227] LEF file: ./Nangate45/Nangate45_tech.lef, created 22 layers, 27 vias
-[WARNING ODB-0217] duplicate VIARULE (Via1Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via1Array-1) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via1Array-2) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via1Array-3) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via1Array-4) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via2Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via2Array-1) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via2Array-2) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via2Array-3) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via2Array-4) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via3Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via3Array-1) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via3Array-2) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via4Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via5Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via6Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via7Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via8Array-0) ignoring...
-[WARNING ODB-0217] duplicate VIARULE (Via9Array-0) ignoring...
-[INFO ODB-0394] Duplicate site FreePDK45_38x28_10R_NP_162NW_34O in Nangate45 already seen in Nangate45_tech
-[INFO ODB-0227] LEF file: ./Nangate45/Nangate45.lef, created 135 library cells
-read lef file ./Nangate45/fakeram45_512x64.lef
-[INFO ODB-0227] LEF file: ./Nangate45/fakeram45_512x64.lef, created 1 library cells
-read lef file ./Nangate45/fakeram45_64x7.lef
-[INFO ODB-0227] LEF file: ./Nangate45/fakeram45_64x7.lef, created 1 library cells
-read lef file ./Nangate45/fakeram45_64x96.lef
-[INFO ODB-0227] LEF file: ./Nangate45/fakeram45_64x96.lef, created 1 library cells
-read_lib file ./Nangate45/fakeram45_512x64.lib
-read_lib file ./Nangate45/fakeram45_64x7.lib
-read_lib file ./Nangate45/fakeram45_64x96.lib
-[WARNING STA-0201] one_cell_nangate45.v line 7, instance ff1 port CL not found.
-[WARNING DFT-0002] Can't scan replace cell 'ff1', that has lib cell 'DFF_X1'. No scan equivalent lib cell found
-No differences found.

src/dft/test/one_cell_nangate45.tcl
@@ -1,36 +1,0 @@
-source "helpers.tcl"
-set LIB_DIR "./Nangate45"
-#
-set tech_lef "$LIB_DIR/Nangate45_tech.lef"
-set std_cell_lef "$LIB_DIR/Nangate45.lef"
-set fake_macro_lef "$LIB_DIR/fakeram45_512x64.lef $LIB_DIR/fakeram45_64x7.lef $LIB_DIR/fakeram45_64x96.lef"
-set liberty_file "$LIB_DIR/Nangate45_fast.lib"
-set fake_macro_lib  "$LIB_DIR/fakeram45_512x64.lib $LIB_DIR/fakeram45_64x7.lib $LIB_DIR/fakeram45_64x96.lib"
- 
-set synth_verilog "./testcases/bp_fe_top.v"
-set floorplan_def "./testcases/bp_fe_top.def"
-set sdc_file "./testcases/bp_fe_top.sdc"
-set top_module "bp_fe_top"
- 
-read_lef $tech_lef
-read_lef $std_cell_lef
-foreach lef_file $fake_macro_lef {
-    puts "read lef file $lef_file"
-    read_lef $lef_file
-
-}
-read_liberty $liberty_file
-foreach lib_file $fake_macro_lib {
-    puts "read_lib file $lib_file"
-    read_liberty $lib_file
-}
- 
-read_verilog one_cell_nangate45.v
-link_design one_cell
-
-scan_replace
-insert_dft
-
-set verilog_file [make_result_file one_cell_nandgate45.v]
-write_verilog $verilog_file
-diff_files $verilog_file one_cell_nandgate45.vok

src/dft/test/one_cell_nangate45.v
@@ -1,13 +1,0 @@
-module one_cell(port1, clock, output1, set_b);
-  input port1;
-  input clock;
-  input set_b;
-  output output1;
-
-  DFF_X1 ff1(
-    .D(port1),
-    .Q(output1),
-    .CL(clock)
-  );
-
-endmodule

cunqinglan/OpenROAD_LRF

commit 88027c4a72106d8ce6f667e98519cd51d178f550

dft: fix null pointer access
Regression introduced in
c950307c1eeffebde769060e68e54340bca3b96e -- physical-only cells without lib views would cause a null pointer access.

Signed-off-by: Mohamed Gaber <me@donn.website>

src/dft/src/cells/ScanCellFactory.cpp
@@ -60,7 +60,8 @@ TypeOfCell IdentifyCell(odb::dbInst* inst, sta::dbSta* sta)
   sta::dbNetwork* db_network = sta->getDbNetwork();
   sta::LibertyCell* liberty_cell
       = GetLibertyCell(inst->getMaster(), db_network);
-  if (liberty_cell->hasSequentials() && !inst->getMaster()->isBlock()) {
+  if (liberty_cell != nullptr && liberty_cell->hasSequentials()
+      && !inst->getMaster()->isBlock()) {
     // we assume that we are only dealing with one bit cells, but in the future
     // we could deal with multibit cells too
     return TypeOfCell::kOneBitCell;