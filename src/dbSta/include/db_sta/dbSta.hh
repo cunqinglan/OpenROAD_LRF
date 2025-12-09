d648bf28dc062e272bb57ecccc9ac2ab633f4c3c
Cunqing Lan <cunqinglan@gmail.com>
2025-12-09 00:53:51 -0800

    delete incresta in dbsta

--- src/dbSta/include/db_sta/dbSta.hh
+++ src/dbSta/include/db_sta/dbSta.hh
@@ -11,7 +11,7 @@
 #include "odb/dbBlockCallBackObj.h"
 #include "odb/dbDatabaseObserver.h"
 #include "sta/Sta.hh"
-#include "sta/IncreSta.hh"
+// #include "sta/IncreSta.hh"
 
 namespace ord {
 class OpenRoad;
@@ -122,7 +122,7 @@ class BufferUseAnalyser
   std::unique_ptr<sta::PatternMatch> clkbuf_pattern_;
 };
 
-class dbSta : public IncreSta, public odb::dbDatabaseObserver
+class dbSta : public Sta, public odb::dbDatabaseObserver
 {
  public:
   dbSta(Tcl_Interp* tcl_interp, odb::dbDatabase* db, utl::Logger* logger);
