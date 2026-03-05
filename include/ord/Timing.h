5f206664d803996505c4f305463116595905554e
Cunqing Lan <cunqinglan@gmail.com>
2026-03-05 04:24:09 -0800

    commit before applying preceding LR cost difference check

--- include/ord/Timing.h
+++ include/ord/Timing.h
@@ -130,6 +130,12 @@ class Timing
                                  bool ratcons = false,
                                  float PT_tradeoff = 100.0,
                                  const char *lr_helper_method = "LRHelper");
+  void testParallelResizeByArrayWithBuffering(size_t max_resize_num,
+                                              size_t iterations,
+                                              size_t num_no_improve_tolerance,
+                                              bool ratcons = false,
+                                              float PT_tradeoff = 100.0,
+                                              const char *lr_helper_method = "RapidLRHelper");
   /////////////////////////////////////////////////////////////
   // End functions for testing LR sizing
   /////////////////////////////////////////////////////////////
