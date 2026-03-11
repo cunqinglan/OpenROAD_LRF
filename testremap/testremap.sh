717c235b841827ba90c1b522cf722ca2a775421f
jzj3021 <980497693@qq.com>
2026-03-11 13:30:47 +0000

    replaced incre gp with dp

--- testremap/testremap.sh
+++ testremap/testremap.sh
@@ -2,9 +2,10 @@
 
 OPENROAD=/home/jzj/physyn/OpenROAD/build/bin/openroad
 SCRIPT=/home/jzj/physyn/OpenROAD/testremap/testscript_input_fp.tcl
-LOG_DIR=/home/jzj/physyn/OpenROAD/testremap/logs
+LOG_DIR=/home/jzj/physyn/OpenROAD/testremap/logs_detailed
 mkdir -p "$LOG_DIR"
-designs=("ac97_top" "aes_cipher_top" "fpu" "NV_NVDLA_partition_m" "NV_NVDLA_partition_p" "NV_NVDLA_partition_p_mod")
+#designs=("ac97_top" "aes_cipher_top" "fpu" "NV_NVDLA_partition_m" "NV_NVDLA_partition_p" "NV_NVDLA_partition_p_mod")
+designs=("ac97_top" "aes_cipher_top" "fpu")
 for design in "${designs[@]}"; do
     echo "Starting: $design"
     DESIGN_NAME="$design" "$OPENROAD" "$SCRIPT" \
